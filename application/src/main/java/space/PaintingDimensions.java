f42f70cd451d0750a9a21614adcf9cd40b4736cf
Michal Lipski <kiwaczki@gmail.com>
2017-03-01 14:25:19 +0100

    created PaintingDimensions.java

---
+++ application/src/main/java/space/PaintingDimensions.java
@@ -0,0 +1,14 @@
+package space;
+
+class PaintingDimensions {
+    private final double centrex;
+    private final double centrey;
+    private final double scale;
+
+    public PaintingDimensions(double centrex, double centrey, double scale) {
+
+        this.centrex = centrex;
+        this.centrey = centrey;
+        this.scale = scale;
+    }
+}
