c79364b70ee35283c7ca791d43560fb81c69106a
Michal Lipski <kiwaczki@gmail.com>
2017-03-01 16:04:14 +0100

    Space creation moved to Space

--- application/src/main/java/space/Space.java
+++ application/src/main/java/space/Space.java
@@ -3,4 +3,43 @@ package space;
 public class Space {
 
 
+    static int createSpace(int width) {
+        int nrOfObjects = 75;
+        if (!SpaceApp.IS_BOUNCING_BALLS) {
+            int stepSize = 3600 * 24 * 7;
+
+            double outerLimit = SpaceConstant.ASTRONOMICAL_UNIT * 20;
+
+            for (int i = 0; i <  nrOfObjects; i++) {
+                double angle = RandomSquareUtil.randSquare() * 2 * Math.PI;
+                double radius = (0.1 + 0.9 * Math.sqrt(RandomSquareUtil.randSquare())) * outerLimit;
+                double weightKilos = 1e3 * SpaceApp.EARTH_WEIGHT * (Math.pow(0.00001 + 0.99999 * RandomSquareUtil.randSquare(), 12));
+                double x = radius * Math.sin(angle);
+                double y = radius * Math.cos(angle);
+                double speedRandom = Math.sqrt(1 / radius) * 2978000*1500 * (0.4 + 0.6 * RandomSquareUtil.randSquare());
+
+                double vx = speedRandom * Math.sin(angle - Math.PI / 2);
+                double vy = speedRandom * Math.cos(angle - Math.PI / 2);
+                SpaceApp.add(weightKilos, x, y, vx, vy, 1);
+            }
+
+            SpaceApp.scale = outerLimit / width;
+
+            SpaceApp.add(SpaceApp.EARTH_WEIGHT * 20000, 0, 0, 0, 0, 1);
+            return stepSize;
+        } else {
+            nrOfObjects = 50;
+            int stepSize = 1;
+            for (int i = 0; i < nrOfObjects; i++) {
+                // radius,weight in [1,20]
+                double radiusAndWeight = 1 + 19 * Math.random();
+                //x,y in [max radius, width or height - max radius]
+                SpaceApp.add(radiusAndWeight, 20 + 760 * Math.random(), 20 + 760 * Math.random(), 3 - 6 * Math.random(), 3 - 6 * Math.random(), radiusAndWeight);
+            }
+            SpaceApp.scale = 1;
+            SpaceApp.centrex = 400;
+            SpaceApp.centrey = 390; //Must compensate for title bar
+            return stepSize;
+        }
+    }
 }
--- application/src/main/java/space/SpaceApp.java
+++ application/src/main/java/space/SpaceApp.java
@@ -82,7 +82,7 @@ public class SpaceApp extends JFrame implements MouseWheelListener,
         spaceApp.addKeyListener(spaceApp);
         spaceApp.setSize(800, 820);
 
-        spaceApp.setStepSize(createSpace(spaceApp.getWidth()));
+        spaceApp.setStepSize(Space.createSpace(spaceApp.getWidth()));
         spaceApp.setVisible(true);
         while (true) {
             final long start = System.currentTimeMillis();
@@ -104,46 +104,6 @@ public class SpaceApp extends JFrame implements MouseWheelListener,
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    private static int createSpace(int width) {
-        int nrOfObjects = 75;
-        if (!IS_BOUNCING_BALLS) {
-            int stepSize = 3600 * 24 * 7;
-
-            double outerLimit = SpaceConstant.ASTRONOMICAL_UNIT * 20;
-
-            for (int i = 0; i <  nrOfObjects; i++) {
-                double angle = RandomSquareUtil.randSquare() * 2 * Math.PI;
-                double radius = (0.1 + 0.9 * Math.sqrt(RandomSquareUtil.randSquare())) * outerLimit;
-                double weightKilos = 1e3 * EARTH_WEIGHT * (Math.pow(0.00001 + 0.99999 * RandomSquareUtil.randSquare(), 12));
-                double x = radius * Math.sin(angle);
-                double y = radius * Math.cos(angle);
-                double speedRandom = Math.sqrt(1 / radius) * 2978000*1500 * (0.4 + 0.6 * RandomSquareUtil.randSquare());
-
-                double vx = speedRandom * Math.sin(angle - Math.PI / 2);
-                double vy = speedRandom * Math.cos(angle - Math.PI / 2);
-                add(weightKilos, x, y, vx, vy, 1);
-            }
-
-            scale = outerLimit / width;
-
-            add(EARTH_WEIGHT * 20000, 0, 0, 0, 0, 1);
-            return stepSize;
-        } else {
-            nrOfObjects = 50;
-            int stepSize = 1;
-            for (int i = 0; i < nrOfObjects; i++) {
-                // radius,weight in [1,20]
-                double radiusAndWeight = 1 + 19 * Math.random();
-                //x,y in [max radius, width or height - max radius]
-                SpaceApp.add(radiusAndWeight, 20 + 760 * Math.random(), 20 + 760 * Math.random(), 3 - 6 * Math.random(), 3 - 6 * Math.random(), radiusAndWeight);
-            }
-            scale = 1;
-            centrex = 400;
-            centrey = 390; //Must compensate for title bar
-            return stepSize;
         }
     }
 
